7488b796970ce2891318e814587e4ff2cb724974
Joseph Senyolo <51865926+TJayCoder@users.noreply.github.com>
2022-03-01 09:24:06 +0200

    Add files via upload

---
+++ document translator/js/main.js
@@ -0,0 +1,155 @@
+$(document).ready(function(){
+
+
+    const videoSrc=$("#player-1").attr("src");
+    $(".video-play-btn , .video-popup").on("click",function(){
+        if($(".video-popup").hasClass("open"))
+        {
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src","");
+        }else{
+            $(".video-popup").addClass("open");
+            if($("#player-1").attr("src")=='')
+            {
+                $("#player-1").attr("src",videoSrc);
+            }
+         
+
+        }
+
+
+    } );
+
+
+});
+
+
+$(window).on("load", function () {
+    $(".preloader").fadeOut("slow");
+});
+
+$(document).ready(function(){
+
+    //=======Features carousel====== 
+$('.features-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+
+//=======Screenshot carousel====== 
+
+$('.screenshot-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:4,
+        }
+    }
+});
+
+
+//=======Testimonials carousel====== 
+
+$('.testimonial-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+
+//=======Team carousel====== 
+
+
+$('.team-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+
+    $.scrollIt({
+        topOffset: -50
+    });
+
+
+
+
+
+    // toogle theme
+
+    function toggleTheme(){
+        updateIcon();
+    }
+    toggleTheme();
+    $(".toggle-theme").on("click",function(){
+        $("body").toggleClass("dark");
+        if($("body").hasClass("dark")){
+            localStorage.setItem("Shala-app", "dark" );
+           
+        }else{
+            localStorage.setItem("Shala-app", "light" );
+        }
+        updateIcon();
+
+    });
+    function updateIcon(){
+        if($("body").hasClass("dark")){
+            $(".toggle-theme i").removeClass("fa-moon");
+            $(".toggle-theme i").addClass("fa-sun");
+        } 
+        else{
+            $(".toggle-theme i").removeClass("fa-sun");
+            $(".toggle-theme i").addClass("fa-moon");
+        }
+    }
+
+
+
+});
